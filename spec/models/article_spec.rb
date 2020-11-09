57c8f9f9b4201000d460552fca3a5b93cc1a9d85
William <willspecker@gmail.com>
2020-11-09 22:23:55 +0100

    Updating database and adding validation for presence of title for Article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,5 @@
 
-Rspec.describe Article, type: :model do
+RSpec.describe Article, type: :model do
     describe 'DB table' do
         it { is_expected.to have_db_column :id }
         it { is_expected.to have_db_column :title }
