c8722ae568d4425fb4c24606e6c81625e626f602
William <willspecker@gmail.com>
2020-11-09 20:56:18 +0100

    Writing first features tests and migrating db

---
+++ spec/features/user_can_see_list_of_articles_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature 'List articles on index page' do
+    context 'with articles in db' do
+        before do
+            visit root_path
+        end
+
+        it 'displays first article title' do
+            expect(page).to have_content 'A breaking news item'
+        end
+
+        it 'displays second article title' do
+            expect(page).to have_content 'Some really breaking action'
+        end
+    end
+end
