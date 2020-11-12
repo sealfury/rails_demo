0f068dacd4266232478720605b90ea795659f31a
William <willspecker@gmail.com>
2020-11-12 13:11:16 +0100

    Defining create method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+  end
+
   def create
+    @article = Article.new(params[:article])
+    @article.save
+    redirect_to @article
   end
 end
