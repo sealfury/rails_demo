b37d1265186af3462707413a5ee30207414d21cc
William <willspecker@gmail.com>
2020-11-14 13:33:32 +0100

    Formatting indents to two spaces

--- spec/features/user_can_see_specific_article_spec.rb
+++ spec/features/user_can_see_specific_article_spec.rb
@@ -1,19 +1,19 @@
 feature 'User can see specific article' do
-    before do
-        create(:article, title: 'A breaking news item', content: 'Some breaking action')
-        create(:article, title: 'Learn Rails 5', content: 'Build awesome rails applications')
+  before do
+    create(:article, title: 'A breaking news item', content: 'Some breaking action')
+    create(:article, title: 'Learn Rails 5', content: 'Build awesome rails applications')
 
-        visit root_path
-        click_on 'A breaking news item'
+    visit root_path
+    click_on 'A breaking news item'
+  end
+
+  context 'Article displays' do
+    it 'title' do
+      expect(page).to have_content 'A breaking news item'
     end
 
-    context 'Article displays' do
-        it 'title' do
-            expect(page).to have_content 'A breaking news item'
-        end
-
-        it 'content' do
-            expect(page).to have_content 'Some breaking action'
-        end
+    it 'content' do
+      expect(page).to have_content 'Some breaking action'
     end
+  end
 end
